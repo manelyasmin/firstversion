042f02e8e81849c64c14183398162942d5499352
manelyasmin <manelyasmine939@gmail.com>
2022-03-12 22:00:24 +0100

    make the animation and fill information of the projects ,the next step will be the resume buttom and the github of portfolio modification also the contact app ending with phone media

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -16,11 +16,7 @@ function Menu({menuItem}) {
                                             <GithubIcon />
                                         </a>
                                     </li>
-                                    <li>
-                                        <a href={item.link2}>
-                                            <GithubIcon />
-                                        </a>
-                                    </li>
+                            
                                 </ul>
                             </div>
                             <h6>{item.title}</h6>
@@ -34,7 +30,104 @@ function Menu({menuItem}) {
 }
 
 const MenuItemStyled=styled.div`
+display:grid;
+grid-template-columns:repeat(3,1fr);
+grid-gap:2rem;
+.grid-item{
+    .portfolio-content{
+        display:block;
+        position:relative;
+        h6{
+            font-size:1.5rem;
 
+        }
+        /* margin-bottom:1.5rem; */
+        img{
+            width:100%;
+            height:30vh;
+            object-fit:cover;
+        }
+        ul{
+            transform:translateY(-600px);
+            transition:all .4s ease-in-out;
+            position:absolute;
+            left:50%;
+            top:40%;
+            opacity:0;
+         li{
+             background-color:var(--border-color);
+             display:flex;
+             align-items: center;
+             justify-content: center;
+             padding:1rem;
+             border-radius:50%;
+             width:3rem;
+             height:3rem;
+             margin :0 .5rem;
+             transition:all .4s ease-in-out;
+             &:hover{
+                 background-color:var(--primary-color);
+             }
+             a{
+                 display:flex;
+                 align-items:center;
+                 justify-content:center;
+                 transition:all .4s ease-in-out;
+             }
+         }   
+        }
+        .portfolio-image{
+            &::before {
+                content:"";
+                position:absolute;
+                left:2%;
+                height:0;
+                width:0;
+                transition:all .4s ease-in-out;
+            }
+        }
+        .portfolio-image:hover{
+            ul{
+transform:translateY(0);
+transform:translateY(-50%,-50%);
+display:flex;
+align-items:center;
+justify-content:center;
+transition:all .4s ease-in-out;
+opacity:1;
+li{
+    transition:all .4s ease-in-out;
+    &:hover{
+        background-color:var(--primary-color);
+    }
+    a{
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        transition:all .4s ease-in-out;
 
+    }
+}
+li:hover{
+    svg{
+       color:var(--white-color);
+    }
+}
+svg{
+    font-size:2rem;
+}
+            }
+            &::before{
+
+                height:calc(100%-32%);
+                width:calc(100%-4%);
+                background-color:white;
+                opacity:.9;
+                transform-origin:left;
+                transition:all .4s ease-in-out;
+            }
+        }
+    }
+}
 `;
 export default Menu
--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -9,46 +9,46 @@ const portfolios = [
         id: 1,
         category: 'CSS',
         image: css1,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'Responsive Resume',
-        text: 'Created with only HTML and CSS.'
+        link1: 'https://github.com/manelyasmin/Data_Mining.git',
+         
+        title: 'Data Mining',
+        text: 'desktop application developed with javafx using unspervised classfication.The algorithm  used in this project apriori,kmeans,clarnas,kmedoids'
     },
     {
         id: 2,
         category: 'Python',
         image: css2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
+        link1: 'https://github.com/manelyasmin/Aablone.git',
+       
+        title: 'Abalone Game',
+        text: 'developping Abalone game and use computer vision to make 2 different intelligence artificel play '
     },
     {
         id: 3,
         category: 'Javascript',
         image: react1,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
+        link1: 'https://github.com/manelyasmin/HAR.git',
+      
+        title: 'supervised learning',
+        text: 'human activites recongnation for live stream'
     },
     {
         id: 4,
         category: 'Animation',
         image: img2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Use Blender',
-        text: 'Free Animation Software'
+        link1: 'https://github.com/manelyasmin/MethaHeurstique.git',
+       
+        title: 'Meta Heurstique',
+        text: 'implimentation with java and javafx the methaheurstique bso and pso and genetic algorithme for solving the 3SAT problem.'
     },
     {
         id: 5,
         category: 'Animation',
         image: img3,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Use Blender',
-        text: 'Free Animation Software'
+        link1: 'https://github.com/manelyasmin/generative_adversiral_networks.git',
+        
+        title: 'generative adversial network',
+        text: 'creation of web application using flask'
     }
 ]
 
